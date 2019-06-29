d6cfc98fb996853f40fd90adaad563151456b242
Ori Sayda <orisayda@gmail.com>
2019-06-29 22:14:19 +0300

    Update CryptoExchangeAPI.ts
    Create binance variable to do calls to binance api

--- CryptoExchangeAPI.ts
+++ CryptoExchangeAPI.ts
@@ -1,29 +1,25 @@
-
-var BinanceAPI = require('./binance/binanceAPI.js');
-
-// -----------------  Binance  -----------------//
+const binanceAPI = require('binance');
 
 
-const sleep2 = ms => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-var binanceApiKeyClient = '';
-var binanceSecretKeyClient = '';
+var api_key = '';
+var secret_key = '';
 
-var Binance = new BinanceAPI(binanceApiKeyClient, binanceSecretKeyClient);
-
-callInventoryBitfinex();
-async function callInventoryBitfinex() {
-    // var binanceDeposit = await Binance.get_deposite_history();
-    // console.log("deposit: ");
-    // console.log(binanceDeposit);
-    // var binanceWithDrawal = await Binance.get_withdraw_history();
-    // console.log("withdrawal: ");
-    // console.log(binanceWithDrawal);
-    var all_symbols_array = await Binance.getAllSymbols();
-    console.log("symbols: ", all_symbols_array);
-    
-    await sleep2(60000);
-    var binanceTrades = await Binance.getAllTrades(all_symbols_array);
-    console.log("trades: ");
-    console.log(binanceTrades);
-}
+const binance = new binanceAPI.BinanceRest({
+      key: api_key, // Get this from your account on binance.com
+      secret: secret_key, // Same for this
+      timeout: 15000, // Optional, defaults to 15000, is the request time out in milliseconds
+      recvWindow: 10000, // Optional, defaults to 5000, increase if you're getting timestamp errors
+      disableBeautification: false,
+      /*
+       * Optional, default is false. Binance's API returns objects with lots of one letter keys.  By
+       * default those keys will be replaced with more descriptive, longer ones.
+       */
+      handleDrift: true
+      /* Optional, default is false.  If turned on, the library will attempt to handle any drift of
+       * your clock on it's own.  If a request fails due to drift, it'll attempt a fix by requesting
+       * binance's server time, calculating the difference with your own clock, and then reattempting
+       * the request.
+       */
+});
